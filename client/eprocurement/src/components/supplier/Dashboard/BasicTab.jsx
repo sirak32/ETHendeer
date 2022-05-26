872cb953b5ca9629276d121d446d4cee8eee61a4
sirak32 <sirak21tesfaye@gmail.com>
2022-05-27 00:18:10 +0300

    stable after we met merid

--- client/eprocurement/src/components/supplier/Dashboard/BasicTab.jsx
+++ client/eprocurement/src/components/supplier/Dashboard/BasicTab.jsx
@@ -9,38 +9,39 @@ import HorizontalLinearStepper from './Stepper';
 import Table from './Table'
 import SupplierTable from './SupplierTable'
 import {FormikFormDemo} from './RegisterOfficer'
-function TabPanel(props) {
-  const { children, value, index, ...other } = props;
+import { TabView, TabPanel } from 'primereact/tabview';
+// function TabPanel(props) {
+//   const { children, value, index, ...other } = props;
 
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
+//   return (
+//     <div
+//       role="tabpanel"
+//       hidden={value !== index}
+//       id={`simple-tabpanel-${index}`}
+//       aria-labelledby={`simple-tab-${index}`}
+//       {...other}
+//     >
+//       {value === index && (
+//         <Box sx={{ p: 3 }}>
+//           <Typography>{children}</Typography>
+//         </Box>
+//       )}
+//     </div>
+//   );
+// }
 
-TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.number.isRequired,
-  value: PropTypes.number.isRequired,
-};
+// TabPanel.propTypes = {
+//   children: PropTypes.node,
+//   index: PropTypes.number.isRequired,
+//   value: PropTypes.number.isRequired,
+// };
 
-function a11yProps(index) {
-  return {
-    id: `simple-tab-${index}`,
-    'aria-controls': `simple-tabpanel-${index}`,
-  };
-}
+// function a11yProps(index) {
+//   return {
+//     id: `simple-tab-${index}`,
+//     'aria-controls': `simple-tabpanel-${index}`,
+//   };
+// }
 
 export default function BasicTabs(props) {
   const [value, setValue] = React.useState(0);
@@ -48,29 +49,46 @@ export default function BasicTabs(props) {
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
+  const [activeIndex, setActiveIndex] = React.useState(0);
 
   return (
-    <Box sx={{ width: '100%',backgroundColor:'white' ,background: "white",borderRadius:'0.7rem' }}>
-      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-        <Tabs  value={value} onChange={handleChange} aria-label="basic tabs example" centered>
-          <Tab label="ALL TENDERS" {...a11yProps(1)} />
-          <Tab label="ADD TENDERS" {...a11yProps(0)} />
-          <Tab label="SUPPLIERS LIST" {...a11yProps(2)} />
-        </Tabs>
-      </Box>
-      <TabPanel value={value} index={1}>
-        <AddTenderForm/>
-        <HorizontalLinearStepper/>
-      </TabPanel>
-      <TabPanel value={value} index={0}>
+
+    // <Box sx={{ width: '100%',backgroundColor:'white' ,background: "white",borderRadius:'0.7rem' }}>
+    //   <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+    //     <Tabs  value={value} onChange={handleChange} aria-label="basic tabs example" centered>
+    //       <Tab label="ALL TENDERS" {...a11yProps(1)} />
+    //       <Tab label="ADD TENDERS" {...a11yProps(0)} />
+    //       <Tab label="SUPPLIERS LIST" {...a11yProps(2)} />
+    //     </Tabs>
+    //   </Box>
+    //   <TabPanel value={value} index={1}>
+    //     <AddTenderForm/>
+    //     <HorizontalLinearStepper/>
+    //   </TabPanel>
+    //   <TabPanel value={value} index={0}>
+    //     <Table data={props.data.OptmTender}/>
+    //   </TabPanel>
+    //   <TabPanel value={value} index={2}>
+    //   </TabPanel>
+    //   <TabPanel value={value} index={2}>
+    //     <SupplierTable data={props.data.suppliers}/>
+    //     <FormikFormDemo/>
+    //   </TabPanel>
+    // </Box>
+
+    <TabView   activeIndex={activeIndex} onTabChange={(e) => setActiveIndex(e.index)}>
+    <TabPanel header="ALL TENDERS">
+        Content I
         <Table data={props.data.OptmTender}/>
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-      </TabPanel>
-      <TabPanel value={value} index={2}>
+    </TabPanel>
+    <TabPanel header="ADD TENDERS">
+        Content II
+        <AddTenderForm/>
+    </TabPanel>
+    <TabPanel header="SUPPLIERS LIST">
+        Content III
         <SupplierTable data={props.data.suppliers}/>
-        <FormikFormDemo/>
-      </TabPanel>
-    </Box>
+    </TabPanel>
+</TabView>
   );
 }
--- client/eprocurement/src/components/supplier/Dashboard/TenderPost.jsx
+++ client/eprocurement/src/components/supplier/Dashboard/TenderPost.jsx
@@ -1,18 +1,35 @@
 import * as React from 'react';
-import Card from '@mui/material/Card';
+// import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
+// import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import { MdArrowDropDown } from 'react-icons/md';
 import { Stack } from '@mui/material';
 import Modal from './Modal'
 import { Link } from 'react-router-dom';
+import { Card } from 'primereact/card';
+import { Button } from 'primereact/button';
+import { Dialog } from 'primereact/dialog';
+import { useState } from 'react';
 export default function MediaCard() {
+  const header = (
+    <img alt="Card" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTm8jXlLuWDxu2tZbT08S5ovq5rj5Tqi8LbTA&usqp=CAU" onError={(e) => e.target.src='https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png'} />
+);
+const footer = (
+    <span>
+        <Button label="Apply" icon="pi pi-check" onClick={(()=>{setVis(true)})} />
+        <Button label="See Detail" icon="pi pi-chevron-circle-down" className="p-button-secondary ml-2"  onClick={(()=>{setVis2(true)})}/>
+    </span>
+
+);
+const [vis,setVis]=useState(false)
+const [vis2,setVis2]=useState(false)
+
   return (
-      
-    <Card sx={{ maxWidth: '33%'}}>
+    <>
+    {/* <Card sx={{ maxWidth: '33%'}}>
         
       <CardMedia
         component="img"
@@ -39,7 +56,24 @@ export default function MediaCard() {
         </Link>
         <Modal><MdArrowDropDown /></Modal> 
       </CardActions>
-    </Card>
+    </Card> */}
+    <div>
+
+            <Card title="ብረታ ብረት" subTitle="Subtitle" style={{ width: '25em' }} footer={footer} header={header}>
+                <p className="m-0" style={{lineHeight: '1.5'}}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deserunt
+                    quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!</p>
+            </Card>
+
+            <Dialog modal draggable={false} dismissableMask style={{width:'90rem',height:'300rem'}} visible={vis} onHide={(()=>{setVis(false)})}>
+                Apply For This Tender Here 
+            </Dialog>
+            <Dialog modal draggable={false} dismissableMask style={{width:'90rem',height:'300rem'}} visible={vis2} onHide={(()=>{setVis2(false)})}>
+                Tender Details Here
+            </Dialog>
+
+
+        </div>
+    </>  
         
   );
 }
